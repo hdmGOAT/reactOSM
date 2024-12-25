1e34bed1e7b34e16b70969c8c7c856e5437cb50b
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2024-12-25 20:22:26 +0800

    cleaned up structure

--- src/components/JeepneyRouter.tsx
+++ src/components/JeepneyRouter.tsx
@@ -49,3 +49,60 @@ const JeepneyRouter = ({
 };
 
 export default JeepneyRouter;
+
+
+/*
+const calculateNearestPointOnRoute = (
+    startPoint: [number, number],
+    route: [number, number][]
+  ): [number, number] => {
+    let minDistance = Infinity;
+    let nearestPoint: [number, number] | null = null;
+
+    for (let i = 0; i < route?.length - 1; i++) {
+      const pointA = route[i];
+      const pointB = route[i + 1];
+
+      const closestPoint = calculateClosestPointOnSegment(
+        startPoint,
+        pointA,
+        pointB
+      );
+
+      const distance = Math.sqrt(
+        Math.pow(closestPoint[0] - startPoint[0], 2) +
+          Math.pow(closestPoint[1] - startPoint[1], 2)
+      );
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestPoint = closestPoint;
+      }
+    }
+
+    return nearestPoint!;
+  };
+  const calculateClosestPointOnSegment = (
+    point: [number, number],
+    segmentStart: [number, number],
+    segmentEnd: [number, number]
+  ): [number, number] => {
+    const [x1, y1] = segmentStart;
+    const [x2, y2] = segmentEnd;
+    const [px, py] = point;
+
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+
+    const t = ((px - x1) * dx + (py - y1) * dy) / (dx * dx + dy * dy);
+
+    const clampedT = Math.max(0, Math.min(1, t));
+
+    return [x1 + clampedT * dx, y1 + clampedT * dy];
+  };
+
+  const nearestPoint = calculateNearestPointOnRoute(
+    start,
+    jeepneyRoutes[0]?.coordinates
+  );
+  */
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,9 @@
-import {
-  MapContainer,
-  TileLayer,
-  Polyline,
-  Tooltip,
-  LayersControl,
-} from "react-leaflet";
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.js";
 import { useEffect, useState } from "react";
-
-import JeepneyRouter from "./JeepneyRouter";
 
 type JeepneyRoute = {
   name: string;
@@ -38,60 +30,6 @@ const Map = () => {
       .catch((error) => console.error("Error fetching JSON:", error));
   }, []);
 
-  const calculateNearestPointOnRoute = (
-    startPoint: [number, number],
-    route: [number, number][]
-  ): [number, number] => {
-    let minDistance = Infinity;
-    let nearestPoint: [number, number] | null = null;
-
-    for (let i = 0; i < route?.length - 1; i++) {
-      const pointA = route[i];
-      const pointB = route[i + 1];
-
-      const closestPoint = calculateClosestPointOnSegment(
-        startPoint,
-        pointA,
-        pointB
-      );
-
-      const distance = Math.sqrt(
-        Math.pow(closestPoint[0] - startPoint[0], 2) +
-          Math.pow(closestPoint[1] - startPoint[1], 2)
-      );
-
-      if (distance < minDistance) {
-        minDistance = distance;
-        nearestPoint = closestPoint;
-      }
-    }
-
-    return nearestPoint!;
-  };
-  const calculateClosestPointOnSegment = (
-    point: [number, number],
-    segmentStart: [number, number],
-    segmentEnd: [number, number]
-  ): [number, number] => {
-    const [x1, y1] = segmentStart;
-    const [x2, y2] = segmentEnd;
-    const [px, py] = point;
-
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-
-    const t = ((px - x1) * dx + (py - y1) * dy) / (dx * dx + dy * dy);
-
-    const clampedT = Math.max(0, Math.min(1, t));
-
-    return [x1 + clampedT * dx, y1 + clampedT * dy];
-  };
-
-  const nearestPoint = calculateNearestPointOnRoute(
-    start,
-    jeepneyRoutes[0]?.coordinates
-  );
-
   return (
     <MapContainer
       center={defaultCenter}
@@ -112,27 +50,6 @@ const Map = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
           />
         </LayersControl.BaseLayer>
-
-        <LayersControl.Overlay checked name="Jeepney Routes">
-          <>
-            {jeepneyRoutes?.map((route, index) => (
-              <Polyline
-                key={index}
-                positions={route.coordinates}
-                color={route.color}
-                weight={4}
-              >
-                <Tooltip permanent>{route.name}</Tooltip>
-              </Polyline>
-            ))}
-          </>
-        </LayersControl.Overlay>
-
-        {nearestPoint && (
-          <LayersControl.Overlay checked name="Routing">
-            <JeepneyRouter start={start} end={nearestPoint} />
-          </LayersControl.Overlay>
-        )}
       </LayersControl>
     </MapContainer>
   );
