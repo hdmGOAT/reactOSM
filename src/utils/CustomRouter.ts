01b2009617a4d563540713b5524897877176a56b
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-18 22:27:17 +0800

    planned api

--- src/utils/CustomRouter.ts
+++ src/utils/CustomRouter.ts
@@ -7,39 +7,37 @@ export class CustomRouter extends L.Class {
     - Create backend api for routing
         -  Json file must contain
         {
-        "code": 
-        "routes": [
+        "code":  // Ok or Error
+        "routes": [ // Array of possible routes
         {
-            "geometry": 
-            "legs":[
-                "steps": [],
-                "summary": "",
-                "weight": 
-                "duration":
-                "distance"
+            "geometry": // Encoded compressed polyline
+            "legs":[ // Breaks down route into segments
+                "steps": [], // Turn by turn instruction of each segment
+                "summary": "", //Describes the leg. Streetnames and shit
+                "weight":  // Cost of the route based on weighing metric (weight_name) used for route selection
+                "duration": // Total travel time in seconds for the leg
+                "distance" // Total distance in meters of the leg
             ],
-            "weight_name":
-            "weight":
-            "duration"
-            "distance"
+            "weight_name": // Specifies weighing metric of the route
+            "weight": // Total cost of the route
+            "duration" // Total travel time in seconds for the route
+            "distance" // Total distance in meters of the route
         }
         ],
         "waypoints": [
-            {
-                "hint"
-                "distance"
-                "name"
-                "location"
-            },
-            {
-                "hint"
-                "distance"
-                "name"
-                "location"
-            },
-        ]
-        
-        
+                {
+                    "hint" // For the server to optimize subsequent reqs
+                    "distance" // Distance from input location to snapped location on road network
+                    "name" // Name of road or location near waypoint
+                    "location" // array of two numbers, coordinates
+                },
+                {
+                    "hint"
+                    "distance"
+                    "name"
+                    "location"
+                },
+            ]
         }
     - 
     - 
